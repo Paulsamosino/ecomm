d774ab37049efabff67694370bbc09e5f7900b43
Paulsamosino <hatchisamosino@gmail.com>
2025-04-27 08:25:00 +0800

    s
    s

--- server/server.js
+++ server/server.js
@@ -9,6 +9,9 @@ require("dotenv").config();
 const app = express();
 const server = http.createServer(app);
 
+// Trust proxy - required for correct IP detection behind Render.com and Cloudflare
+app.set("trust proxy", true);
+
 // Basic middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
